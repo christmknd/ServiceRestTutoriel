be0d2a54cb401f03e01fdf86f9269b9a0ddd988b
christmknd <christmokonda@gmail.com>
2026-05-26 15:39:09 +0200

    Restart client app

--- ClientSOAP/src/main/java/com/mycompany/clientsoap/ClientSOAP.java
+++ ClientSOAP/src/main/java/com/mycompany/clientsoap/ClientSOAP.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.javaapplicationcliente;
+package com.mycompany.clientsoap;
 
 /**
  *
  * @author user
  */
-public class JavaApplicationCliente {
+public class ClientSOAP {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
